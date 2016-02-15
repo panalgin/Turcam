6bb4605cf8fff44b732199663f7e4e548900a6dd
panalgin <mashadow@outlook.com>
2016-02-15 12:20:45 +0200

    Project initialize :=)

---
+++ Turcam/JavascriptInteractionController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Turcam
+{
+    public class JavascriptInteractionController
+    {
+        public JavascriptInteractionController()
+        {
+
+        }
+
+        public void Exit()
+        {
+            Application.Current.Dispatcher.BeginInvoke(new Action( delegate ()
+            {
+                Application.Current.Shutdown();
+            }));
+        }
+
+        public void ToggleWindowState()
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() => {
+                if (Application.Current.MainWindow.WindowState == WindowState.Normal)
+                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                else if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+
+            }), DispatcherPriority.ContextIdle);
+        }
+
+        public void MinimizeWindowState()
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Minimized;
+            }));
+        }
+    }
+}
