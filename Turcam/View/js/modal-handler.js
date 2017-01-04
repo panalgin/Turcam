1263a1eb1a5375778a962af09cc95b4909a2195c
panalgin <mashadow@outlook.com>
2017-01-04 11:15:29 +0300

    clear from the day before

---
+++ Turcam/View/js/modal-handler.js
@@ -0,0 +1,22 @@
+﻿function createModal(header, path, type) {
+
+    var container = "<div class='modal'><div class='header'></div><div class='content'></div></div>";
+    container = $("<div/>").html(container).contents();
+
+    if (type == undefined) {
+        $.get(path, function (html) {
+            container.children("div.header").append(header);
+            container.children("div.content").append(html);
+            container.modal({ closeClass: 'modal-close', closeText: '' });
+        });
+    }
+
+    if (type !== undefined) {
+        if (type == "Question") {
+
+        }
+        else if (type == "Error") {
+
+        }
+    }
+}
