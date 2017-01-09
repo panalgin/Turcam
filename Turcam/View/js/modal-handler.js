7519039d230c8ea6144ef257c2666931d75cb025
panalgin <mashadow@outlook.com>
2017-01-09 15:26:52 +0300

    deleting of drillbits, ui ready

--- Turcam/View/js/modal-handler.js
+++ Turcam/View/js/modal-handler.js
@@ -1,26 +1,25 @@
 ﻿function createModal(header, path, data, type) {
-
     var container = "<div class='modal'><div class='header'></div><div class='content'></div></div>";
     container = $("<div/>").html(container).contents();
+    
+    $.get(path, function (html) {
+        container.children("div.header").append(header);
+        container.children("div.content").append(html);
 
-    if (type === undefined) {
-        $.get(path, function (html) {
-            container.children("div.header").append(header);
-            container.children("div.content").append(html);
+        if (data)
+            container.find("input#actual-item").data("drill", data);
 
-            if (data)
-                container.find("input#actual-item").data("drill", data);
-
-            container.modal({ closeClass: 'modal-close', closeText: '' });
-        });
-    }
+        container.modal({ closeClass: 'modal-close', closeText: '' });
+    });
 
     if (type !== undefined) {
+        console.log("4üncü parametreye girdi;")
         if (type === "Question") {
-
+            console.log("Questiona girdi.");
+            container.addClass("question");
         }
         else if (type === "Error") {
-
+            container.addClass("error");
         }
     }
 }
